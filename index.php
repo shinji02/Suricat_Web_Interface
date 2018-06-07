283d0b7f25827bfc9da40ece22d06b3159a4b5f0
Arisa <jovannserreau@gmail.com>
2018-06-07 11:20:14 +0200

    Création de la partie de connection

--- index.php
+++ index.php
@@ -10,11 +10,49 @@
     
     $name_web = $bdd->getvarweb();
     
-    $array = array(
-      "web_name" => $name_web['web_name']  
-    );
-    
-    $Smarty->assign('var',$array);
-    
-    $Smarty->display('login.tpl');
+    if(isset($_GET['login'])){
+        if(!empty($_POST['pseudo']) && !empty($_POST['password']))
+        {            
+            $login = htmlspecialchars($_POST['pseudo']);
+            $mdp = sha1($_POST['password']);       
+            $rsp = $bdd->user_auth($login, $mdp);
+            if($rsp==1){
+                $array = array(
+                    'web_name' => $name_web['web_name'],
+                    'sucess' => array('sucessn' => 1,
+                                    'msg' => 'Connection réussie')
+                );
+                $Smarty->assign('var',$array); 
+            }
+            else
+            {
+                $array = array(
+                    'web_name' => $name_web['web_name'],
+                    'error' => array('errorn' => 1,
+                                    'msg' => 'Utilisateur ou mot de passe incorrect')
+                );
+                $Smarty->assign('var',$array); 
+            }
+        }
+        else
+        {
+            $array = array(
+                'web_name' => $name_web['web_name'],
+                'error' => array('errorn' => 1,
+                                 'msg' => 'Vous avez pas remplie tout les champs')
+            );
+            $Smarty->assign('var',$array);
+        }
+        $Smarty->display('login.tpl');
+    }
+    else
+    {  
+        $array = array(
+          "web_name" => $name_web['web_name']  
+        );
+
+        $Smarty->assign('var',$array);
+
+        $Smarty->display('login.tpl');
+    }
 ?>
