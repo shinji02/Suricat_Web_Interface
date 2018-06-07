de3c5000f0a39f5cdd0f53738dbb2789c6ff11e1
Arisa <jovannserreau@gmail.com>
2018-06-07 10:49:17 +0200

    Création de smarty

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
     require 'classe/C_BDD.php';
     $bdd = new C_BDD();
-    $bdd->connect();
+    //Imporatation de classe C_BDD.php
+    require 'libs/Smarty.class.php';
+    
+    $Smarty = new Smarty();
+    $Smarty->compile_dir ='tmp';
+    $Smarty->template_dir = 'templates';
+    
+    $Smarty->display('login.tpl');
 ?>
